a065bb8b697c218a334115c98c20400b5f77f7a2
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-10-08 15:54:07 +0530

    bugs

--- api/server.js
+++ api/server.js
@@ -5,3 +5,8 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/mern-todo",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true 
+}).then(()=> console.log("Connected Successfully !");)
