22f833baf3319786c02e030620c1a406cca79e1a
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-10-08 18:29:58 +0530

    Love the UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,22 @@
+import { useState , useEffect } from 'react';
+const API_BASE =  "http://localhost:3001";
+
 function App() {
+  const [todos,setTodos]=useState([]);
+  const [popupActive,setPopupActive]=useState(false);
+  const [newTodo,setNewTodo]=useState("");
+
+  useEffect( ()=>{
+    GetTodos();
+  },[] );
+
+  const GetTodos = ()=>{
+    fetch(API_BASE + "/todos")
+    .then(res =>res.json())
+    .then( data => setTodos(data))
+    .then(err => console.error("Error : " , err));
+  }
+
   return (
     <div className="App">
       <h1>Welcome, buddy!</h1>
